ccacb3388d6187e1bb5b727630cc18c6788c2d2e
davidecaputo <daca9621@gmail.com>
2023-03-29 20:26:02 +0200

    remove active

--- js/script.js
+++ js/script.js
@@ -14,25 +14,25 @@ createApp({
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    class: 'slide active',
+                    class: 'slide',
                     classPreview: 'preview',
                 }, {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    class: 'slide active',
+                    class: 'slide',
                     classPreview: 'preview',
                 }, {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    class: 'slide active',
+                    class: 'slide',
                     classPreview: 'preview',
                 }, {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    class: 'slide active',
+                    class: 'slide',
                     classPreview: 'preview',
                     classImage: 'avengers'
                 }
